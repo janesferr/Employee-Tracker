f305aa638d261cc6d075df842d402f58c74fe54b
jansferr <sferrazzajane@gmail.com>
2021-02-14 19:32:04 -0500

    Added the prompt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { read } = require('fs/promises');
 const mysql = require('mysql');
 const cTable = require('console.table');
+var inquirer = require('inquirer');
 
 //connection to the mysql,port, password, and database:
 const connection = mysql.createConnection({
@@ -16,12 +17,48 @@ const readProducts = () => {
     console.log('Selecting all Employees...');
     connection.query('Select * from employee', (err, res) => {
         if(err) throw err;
-        // console.log(res);
+
         console.table(res);
         connection.end();
         
     });
 };
-readProducts();
 
 
+
+function displayPrompt() {
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          message: "What would  you like to do?",
+          name: 'todoList',
+          choices: ["View All Employees",
+           "View All Employees By Department", 
+          "View All Employees By Manager",
+          "Add Employee",
+          "Add Employee",
+          "Remove Employee",
+          "Update Employee Role",
+          "Update Employee Manager",
+          "View All Roles",
+          "Add Role",
+          "Remove Role",
+          "View All Department",
+          "Add Department",
+          "Remove Department",
+          "Quit",
+        ],
+          validate: (value) => { if (value) { return true } else { return " I need a value to continue" } },
+  
+        }])
+      .then(function ({ todoList }) {
+        if (todoList === "View All Employees") {readProducts(); }
+        else if(todoList === "Quit"){  console.log("Goodbye");}
+        else{
+            console.log("An error has occured!");
+        }
+    });
+
+}
+displayPrompt();
